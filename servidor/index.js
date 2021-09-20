60f42999c2aca0032ae51e8e2b08464d6bd14ee5
lopohalo <79216367+lopohalo@users.noreply.github.com>
2021-09-20 14:38:05 -0500

    cambio de puerto

--- servidor/index.js
+++ servidor/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use( express.json({ extended: true }));
 
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 
 app.use('/api/usuarios', require('./routes/usuarios') )
@@ -22,6 +22,6 @@ app.use('/api/proyectos', require('./routes/proyectos') )
 app.use('/api/tareas', require('./routes/tareas') )
 
 
-app.listen(PORT, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log( `sofi linda ${PORT}`);
 })
